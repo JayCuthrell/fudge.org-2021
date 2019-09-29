d8d53e8a5613bb89435881c3bfd2539cb718b61b
JayCuthrell <jay@cuthrell.com>
2019-09-28 19:24:46 -0500

    making emoji codes work again

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,13 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          {
+            resolve: `gatsby-remark-emoji`,
+            options: {
+              emojiConversion: `shortnameToUnicode`,
+              ascii: false,
+            },
+          },
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
