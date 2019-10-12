41dfeda48ad5905f6e2673b33bc656785af1bdeb
JayCuthrell <jay@cuthrell.com>
2019-10-12 14:38:28 -0500

    npx react-codemod

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,6 @@ import userConfig from '../../../config';
 import Container from '../Container';
 import HeaderImage from '../HeaderImage';
 import Social from '../Social';
-import H1 from '../H1';
 import P from './P';
 import Link from './Link';
 import Wrapper from './Wrapper';
@@ -18,8 +17,7 @@ function Header({ config }) {
         {userConfig.showHeaderImage && (
           <HeaderImage/>
         )}
-        <H1><Link to="/">{author}</Link></H1>
-        <P>{description}</P>
+        <P><Link to="/">{description} by {author}</Link></P>
         {social &&
           <Social
             website={social.website}
