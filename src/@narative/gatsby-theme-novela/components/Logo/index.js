7785ee5b5152ebaadf985d98a06b6c9ba28b8147
JayCuthrell <jay@cuthrell.com>
2019-10-20 19:42:31 -0500

    redirect update and smaller logo

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -2,24 +2,6 @@ import React from 'react';
 
 export default function Logo() {
   return (
-    
-<svg width="192" height="23" version="1.1" viewBox="0 0 50.8 6.0854167" xmlns="http://www.w3.org/2000/svg">
- <g transform="translate(0,-290.91458)">
-  <g fill="#000000" font-family="sans-serif" letter-spacing="0px" word-spacing="0px">
-   <text x="40.821426" y="104.98809" font-size="10.58333302px" stroke-width=".26458332" style="line-height:1.25" xml:space="preserve"><tspan x="40.821426" y="114.35186" stroke-width=".26458332"/></text>
-   <flowRoot transform="scale(.26458333)" font-size="40px" style="line-height:1.25" xml:space="preserve"><flowRegion><rect x="182.85715" y="488.23398" width="300" height="165.71428"/></flowRegion><flowPara/></flowRoot>
-   <text transform="scale(.99008223 1.0100171)" x="0.025194852" y="292.61163" font-size="12.84729576px" stroke-width=".3211824" style="line-height:1.25" xml:space="preserve"><tspan x="0.025194852" y="292.61163" font-family="Dyuthi" font-size="6.85189152px" stroke-width=".3211824" style="font-feature-settings:normal;font-variant-caps:normal;font-variant-ligatures:normal;font-variant-numeric:normal">fudge.org</tspan></text>
-  </g>
-  <g fill-rule="evenodd">
-   <path d="m144.45647 264.91257v17.451l17.81021 2.7491v-11.59463z" points="144.45647,282.36357 162.26668,285.11267 162.26668,273.51804 144.45647,264.91257 " fill="#353564"/>
-   <path d="m162.26668 273.51804 8.89766-4.56537v12.59809l-8.89766 3.56191z" points="171.16434,268.95267 171.16434,281.55076 162.26668,285.11267 162.26668,273.51804 " fill="#e9e9ff"/>
-   <path d="m144.45647 264.91257 11.578-8.35757 15.12987 12.39767-8.89766 4.56537z" points="156.03447,256.555 171.16434,268.95267 162.26668,273.51804 144.45647,264.91257 " fill="#4d4d9f"/>
-   <path d="m144.45647 282.36357 11.578-5.98053 15.12987 5.16772-8.89766 3.56191z" points="156.03447,276.38304 171.16434,281.55076 162.26668,285.11267 144.45647,282.36357 " fill="#afafde"/>
-   <path d="m156.03447 256.555v19.82804l15.12987 5.16772v-12.59809z" points="156.03447,276.38304 171.16434,281.55076 171.16434,268.95267 156.03447,256.555 " fill="#d7d7ff"/>
-   <path d="m144.45647 264.91257 11.578-8.35757v19.82804l-11.578 5.98053z" points="156.03447,256.555 156.03447,276.38304 144.45647,282.36357 144.45647,264.91257 " fill="#8686bf"/>
-  </g>
-  <text x="32.779533" y="296.34225" fill="#000000" font-family="sans-serif" font-size="10.58333302px" letter-spacing="0px" stroke-width=".26458332" word-spacing="0px" style="line-height:1.25" xml:space="preserve"><tspan x="32.779533" y="296.34225" font-family="Lato" font-size="2.82222223px" stroke-width=".26458332" style="font-feature-settings:normal;font-variant-caps:normal;font-variant-ligatures:normal;font-variant-numeric:normal">by Jay Cuthrell</tspan></text>
- </g>
-</svg>
+<svg width="313.6325439453125px" height="53.6175048828125px" xmlns="http://www.w3.org/2000/svg" viewBox="93.18372802734376 48.19124755859375 313.6325439453125 53.6175048828125" preserveAspectRatio="xMidYMid"><defs><filter id="editing-extrusion" x="-100%" y="-100%" width="300%" height="300%"><feFlood result="color1" flood-color="#444"></feFlood><feConvolveMatrix order="8,8" divisor="1" kernelMatrix="1 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 1" in="SourceAlpha" result="extrude"></feConvolveMatrix><feComposite in="color1" in2="extrude" result="comp-extrude" operator="in"></feComposite><feOffset dx="4" dy="4" in="comp-extrude" result="offset-extrude"></feOffset><feMerge><feMergeNode in="offset-extrude"></feMergeNode><feMergeNode in="SourceGraphic"></feMergeNode></feMerge></filter></defs><g filter="url(#editing-extrusion)"><g transform="translate(142, 86.640625)"><path d="M15-3L9-3L9-15L3-15L3-18L9-18L9-21L12-21L12-24L21-24L21-21L15-21L15-18L21-18L21-15L15-15L15-3ZM45-3L27-3L27-6L24-6L24-18L30-18L30-6L39-6L39-18L45-18L45-3ZM69-3L51-3L51-6L48-6L48-15L51-15L51-18L63-18L63-24L69-24L69-3ZM54-15L54-6L63-6L63-15L54-15ZM90 0L75 0L75-3L87-3L87-6L75-6L75-9L72-9L72-15L75-15L75-18L93-18L93-3L90-3L90 0ZM78-15L78-9L87-9L87-15L78-15ZM114-3L99-3L99-6L96-6L96-15L99-15L99-18L114-18L114-15L117-15L117-9L102-9L102-6L114-6L114-3ZM102-15L102-12L111-12L111-15L102-15ZM132-3L126-3L126-9L132-9L132-3ZM162-3L147-3L147-6L144-6L144-15L147-15L147-18L162-18L162-15L165-15L165-6L162-6L162-3ZM150-15L150-6L159-6L159-15L150-15ZM177-18L177-15L180-15L180-12L177-12L177-3L171-3L171-18L177-18ZM180-18L189-18L189-15L180-15L180-18ZM210 0L195 0L195-3L207-3L207-6L195-6L195-9L192-9L192-15L195-15L195-18L213-18L213-3L210-3L210 0ZM198-15L198-9L207-9L207-15L198-15Z" fill="#ccc"></path></g></g></svg>
   );
 }
