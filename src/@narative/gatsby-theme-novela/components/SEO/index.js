95606d01f73d80f83d536c83615118b1637eca5e
Jay Cuthrell <jay@cuthrell.com>
2020-10-01 11:54:46 -0400

    SEO override for favicon default attempt 3

--- src/@narative/gatsby-theme-novela/components/SEO/index.js
+++ src/@narative/gatsby-theme-novela/components/SEO/index.js
@@ -2,14 +2,6 @@ import React from 'react';
 
 export default function SEO() {
   return (
-  image = image ? image : `${site.siteUrl}/icons/icon-512x512.png`;
-
-  // Checks if the source of the image is hosted on Contentful
-  if (`${image}`.includes('ctfassets')) {
-    image = `${image}`;
-  } else {
-    image = fullURL(image);
-  }
-
+  image = image ? image : `${site.siteUrl}/icons/icon-512x512.png`
   );
 };
